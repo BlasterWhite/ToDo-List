ea1292c2ad32f233710cb1a82cd4523395f26787
Matéo G <icarambala@gmail.com>
2022-07-03 17:20:28 +0200

    V-cloak | Store in Local Tasks
    Hide VueJS during Loading.
    Store the array in the LocalStorage

--- app.js
+++ app.js
@@ -9,21 +9,24 @@ const app = createApp({
             description: ''
         }
     }, 
-    async mounted() {
-        const res = await fetch('./tasks.json');
+    mounted() {
+        /*const res = await fetch('./tasks.json');
         const data = await res.json();
-        this.tasks = data;
+        this.tasks = data;*/
+        this.tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
     },
     methods: {
         deleteTask(t) {
             this.tasks = this.tasks.filter(task => task.id !== t.id);
+            this.updateLocalStorage();
         },
         
         doneTask(t) {
             this.tasks = this.tasks.filter(task => {
                 if (task.id === t.id) {
                     task.done = true;
-                }
+                }                
+                this.updateLocalStorage();
                 return task;
             });
         },
@@ -33,6 +36,7 @@ const app = createApp({
                 if (task.id === t.id) {
                     task.done = false;
                 }
+                this.updateLocalStorage();
                 return task;
             });
         },
@@ -42,18 +46,23 @@ const app = createApp({
                 alert('You must fill all fields');
             } else {
                 this.tasks.push({
-                    id: this.tasks.length + 1,
+                    id: Date.now(),
                     title: this.title,
                     description: this.description,
                     done: false
                 });
                 this.title = '';
                 this.description = '';
+                this.updateLocalStorage();
             }
+        },
+
+        updateLocalStorage() {
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
         }
     }
-})
-
+    
+});
 
 
 app.mount('#app')
